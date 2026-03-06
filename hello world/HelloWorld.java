be02bc78a14c0bd1650cdfc46bbd134ccbd5c982
paranoia52 <1142920583@qq.com>
2026-03-06 14:46:43 +0800

    test

---
+++ hello world/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		for(int i=1;i<5;i++) {
+			System.out.println("HelloWorld i = " + i);
+		}
+		
+		/*Scanner sc = new Scanner(System.in);
+		System.out.println("plealse shuru");
+		int c = sc.nextInt();
+		System.out.println("shuru = " + c); */
+		
+		int[] arr = {1,2,3};
+		System.out.print("数组1 = " + arr);
+	}
+	
+}
